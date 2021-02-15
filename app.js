5113a03768e8551ebf26e15dbbbd0d02bf03434d
Diana García <diana.eli.gg@gmail.com>
2021-02-14 23:34:42 -0600

    root route

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var db = require("./models");
 
 app.get("/", (req, res) => {
   console.log("========== / ============");
-  res.send("HI");
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.get("/api/bills", (req, res) => {
