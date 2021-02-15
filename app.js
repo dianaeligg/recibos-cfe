49adbdf21410c41530783e941820569f348aa5dc
Diana García <diana.eli.gg@gmail.com>
2021-02-14 23:37:59 -0600

    path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
