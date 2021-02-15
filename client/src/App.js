4334d85105fef5615751659d6ea70811990d1f68
Diana García <diana.eli.gg@gmail.com>
2021-02-14 22:23:43 -0600

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import Places from "./components/places";
-import BillInput from "./components/bill-input";
-import BillTable from "./components/bill-table";
-import "./App.css";
-
-const queryClient = new QueryClient();
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./pages/main";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Captura de Recibos</h1>
-        <Places />
-        <BillInput />
-        <BillTable />
-      </div>
-    </QueryClientProvider>
+    <Router>
+      <Route exact path="/" component={Main} />
+    </Router>
   );
 }
 
---
+++ client/src/pages/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Places from "../components/places";
+import BillInput from "../components/bill-input";
+import "./main.css";
+import BillTable from "../components/bill-table";
+
+const queryClient = new QueryClient();
+
+function Main() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="Main">
+        <h1>Captura de Recibos</h1>
+        <Places />
+        <BillInput />
+        <BillTable />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default Main;
